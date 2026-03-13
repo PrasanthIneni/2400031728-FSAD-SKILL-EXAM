80fb85ba517096412ee3cbac027e08d2d714cf53
I PRASANTH <ineniprasanth@gmail.com>
2026-03-13 10:11:43 +0530

    Delete src/main/java/com/klef/fsad/exam directory

--- src/main/java/com/klef/fsad/exam/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klef.fsad.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klef.fsad.exam.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-
-}
